26d0c041590a567b6a0f21b7729c9170e4ddae39
BMCC-CSC350-SP21-G <81182606+BMCC-CSC350-SP21-G@users.noreply.github.com>
2021-05-24 14:56:16 -0400

    Add files via upload

---
+++ Deliverables/tables.sql
@@ -0,0 +1,13 @@
+create schema login;
+
+create table Users(
+Username varchar(45), 
+Password varchar(45));
+
+-- for first timers
+-- users (kok, kim, king)
+-- password (enter any new password)
+
+create table Appointments(
+date date, 
+time time);
